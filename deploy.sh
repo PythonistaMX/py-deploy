8b2ef62f75aef31ad30e74439b69b9c5707edf1a
Jose Luis Chiquete <josech@gmail.com>
2020-03-22 06:33:55 +0000

    200322a

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+sudo apt update 
+sudo apt dist-upgrade -y
+sudo apt install apache2 mysql-server php-zip zip  php-curl php-mysql tree mc vim libapache2-mod-php python3-dev unzip wget w3m build-essential -y 
+sudo apt purge cloud-init -y 
+sudo apt clean
+wget https://bootstrap.pypa.io/get-pip.py
+sudo -H python3 get-pip.py
+rm get-pip.py
+sudo -H pip install notebook bash-kernel
+python3 -m bash_kernel.install
+jupyter notebook --generate-config
+cp jupyter_notebook_config.json $HOME/.jupyter/
+sudo mkdir /opt/oi
+sudo chown -R oi:oi /opt/oi
+echo -e "#! /bin/bash\nsource $HOME/.nvm/nvm.sh\njupyter notebook --ip=0.0.0.0 --no-browser" > $HOME/jupyter.sh
+chmod +x $HOME/jupyter.sh
+echo -e "[Unit]\nDescription=Jupyter Notebook\n\n[Service]\nType=simple\nPIDFile=/run/jupyter.pid\nExecStart=/home/oi/jupyter.sh\nUser=oi\nGroup=oi\nWorkingDirectory=/opt/oi/\nRestart=always\nRestartSec=10\n\n[Install]\nWantedBy=multi-user.target" > jupyter.service
+sudo mv jupyter.service /lib/systemd/system/
+sudo systemctl enable jupyter.service
+wget https://github.com/vrana/adminer/releases/download/v4.7.6/adminer-4.7.6.php
+sudo mv adminer-4.7.6.php /var/www/html/adminer.php
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 10
+npm install -g yarn
+npm install -g ijavascript
+ijsinstall
+mkdir $HOME/ide
+cp package.json $HOME/ide/
+cd $HOME/ide/
+yarn
+yarn theia build
+yarn theia download:plugins
+echo "export THEIA_DEFAULT_PLUGINS=local-dir:$HOME/ide/plugins" >> $HOME/.bashrc
+export THEIA_DEFAULT_PLUGINS=local-dir:$HOME/ide/plugins
+echo -e "#! /bin/bash\nsource $HOME/.nvm/nvm.sh\ncd $HOME/ide\nyarn theia start /opt/oi --hostname=0.0.0.0" > $HOME/theia.sh
+chmod +x $HOME/theia.sh
+echo -e "[Unit]\nDescription=Theia IDE\n\n[Service]\nType=simple\nPIDFile=/run/theia.pid\nEnvironment=THEIA_DEFAULT_PLUGINS=local-dir:/home/oi/ide/plugins\nExecStart=/home/oi/theia.sh\nUser=oi\nGroup=oi\nWorkingDirectory=/home/oi/ide/\nRestart=always\nRestartSec=10\n\n[Install]\nWantedBy=multi-user.target" > $HOME/theia.service
+sudo mv $HOME/theia.service /lib/systemd/system/
+sudo systemctl enable theia.service
