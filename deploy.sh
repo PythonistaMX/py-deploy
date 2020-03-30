883d2ed326d3728b4589dfcb7839c022b66cbf5b
Jose Luis Chiquete <josech@gmail.com>
2020-03-29 19:25:21 -0600

    200329f

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | b
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install 12
+nvm install 13
 npm install -g yarn
 npm install -g ijavascript
 ijsinstall
