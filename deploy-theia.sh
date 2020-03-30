5ceee950780db88420f65288231952e0a0e22566
Jose Luis Chiquete <josech@gmail.com>
2020-03-29 19:35:25 -0600

    200329g

--- deploy-theia.sh
+++ deploy-theia.sh
@@ -22,7 +22,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | b
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install 11
+nvm install 10
 npm install -g yarn
 
 npm install -g ijavascript
